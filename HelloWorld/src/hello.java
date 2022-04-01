be292107a88cd40ed7031fa634009fe569584c2b
김민호 <kr545@naver.com>
2022-04-02 06:12:48 +0900

    eclipse 연동 첫번째 Test

---
+++ HelloWorld/src/hello.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class hello {
+	public static void main(String[] arge) {
+//		int x = 30;
+//		x = 40;
+//		final int y = 40;
+//		System.out.println(x);
+//		System.out.println(y);
+		Scanner z = new Scanner(System.in);
+		
+		String str = z.next();
+		int i = z.nextInt();
+		long l = z.nextLong();
+		System.out.println(str);
+	}
+}
---
+++ HelloWorld/src/if_test.java
@@ -0,0 +1,18 @@
+
+public class if_test {
+
+	public static void main(String[] args) {
+		int i = 5;
+		
+		if (i<6) {
+			System.out.println("작음");
+		} else if (i<3){
+			System.out.println("더작음");
+		}
+		else {
+			System.out.println("나머지");
+		}
+
+	}
+
+}
